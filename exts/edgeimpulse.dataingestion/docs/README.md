f28c6ad11db57e5df11041a35e82fd50a7388271
Jenny Plunkett <jenny@edgeimpulse.com>
2023-04-22 09:00:22 +0200

    Initial commit

---
+++ exts/edgeimpulse.dataingestion/docs/README.md
@@ -0,0 +1,3 @@
+# Edge Impulse Data Ingestion
+
+NVIDIA Omniverse extension to upload data directly into your Edge Impulse project.
---
+++ exts/edgeimpulse.dataingestion/edgeimpulse/dataingestion/extension.py
@@ -0,0 +1,121 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import omni.ext
+import omni.ui as ui
+import asyncio
+import aiohttp
+from omni.ui import style_utils
+from functools import partial
+import requests
+import os
+
+async def upload_data(api_key, data_folder, dataset):
+    dataset_types = ["training", "testing", "anomaly"]
+    if dataset not in dataset_types:
+        return('Error: Dataset type invalid (must be training, testing, or anomaly).')
+    url = 'https://ingestion.edgeimpulse.com/api/' + dataset + '/files'
+    try:
+        for file in os.listdir(data_folder):
+            file_path = os.path.join(data_folder, file)
+            # Labels are determined from the filename, anything after "." is ignored, i.e.
+            # File "object.1.blah.png" will be uploaded as file object.1.blah with label "object"
+            label = os.path.basename(file_path).split('.')[0]
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as file:
+                    res = requests.post(url=url,
+                    headers= {
+                        'x-label': label,
+                        'x-api-key': api_key,
+                        'x-disallow-duplicates': '1'
+                    },
+                    files = { 'data': (os.path.basename(file_path), open(file_path, 'rb'), 'image/png') }
+                )
+            if (res.status_code == 200):
+                return(res.text)
+            else:
+                return(str("Error: Status Code " + str(res.status_code) + ": " + res.text))
+    except FileNotFoundError:
+        return('Error: Data Path invalid.')
+
+def getPath(label, text):
+    DataIngestion.DATA_FOLDER = os.path.normpath(text)
+    #print("Data Path Changed:", DataIngestion.DATA_FOLDER)
+
+def getEIAPIKey(label, text):
+    DataIngestion.API_KEY = text
+    #print("Edge Impulse API Key Changed:", DataIngestion.API_KEY)
+
+def getDatasetType(label, text):
+    DataIngestion.DATASET = text
+    #print("Edge Impulse API Key Changed:", DataIngestion.DATASET)
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class DataIngestion(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    
+    API_KEY = ''
+    DATA_FOLDER = ''
+    DATASET = 'training'
+
+    def on_startup(self, ext_id):
+        print("[edgeimpulse.dataingestion] Edge Impulse Data Ingestion startup")
+
+        self._window = ui.Window("Edge Impulse Data Ingestion", width=450, height=220)
+        with self._window.frame:
+            with ui.VStack(spacing=8):
+                
+                with ui.HStack(height=20):
+                    ui.Spacer(width=3)
+                    ui.Label("Create a free Edge Impulse account: https://studio.edgeimpulse.com/", height=20, word_wrap=True)
+                    ui.Spacer(width=3)
+
+                with ui.HStack(height=20):
+                    ui.Spacer(width=3)
+                    data_path_label = ui.Label("Data Path", width=70)
+                    ui.Spacer(width=8)
+                    data_path = ui.StringField(name="path")
+                    data_path.model.set_value("C:\\Users\\...")
+                    data_path.model.add_value_changed_fn(lambda m, label=data_path_label: getPath(data_path_label, m.get_value_as_string()))
+                    ui.Spacer(width=3)
+
+                with ui.HStack(height=20):
+                    ui.Spacer(width=3)
+                    ei_api_key_label = ui.Label("API Key", width=70)
+                    ui.Spacer(width=8)
+                    ei_api_key = ui.StringField(name="ei_api_key")
+                    ei_api_key.model.set_value("ei_02162...")
+                    ei_api_key.model.add_value_changed_fn(lambda m, label=ei_api_key_label: getEIAPIKey(ei_api_key_label, m.get_value_as_string()))
+                    ui.Spacer(width=3)
+
+                with ui.HStack(height=20):
+                    ui.Spacer(width=3)
+                    dataset_type_label = ui.Label("Dataset", width=70)
+                    ui.Spacer(width=8)
+                    dataset_type = ui.StringField(name="dataset")
+                    dataset_type.model.set_value("training, testing, or anomaly (default = training)")
+                    dataset_type.model.add_value_changed_fn(lambda m, label=dataset_type_label: getDatasetType(dataset_type_label, m.get_value_as_string()))
+                    ui.Spacer(width=3)
+
+                def on_click():
+                    #asyncio.ensure_future()
+                    loop = asyncio.get_event_loop()
+                    res = loop.run_until_complete(upload_data(self.API_KEY, self.DATA_FOLDER, self.DATASET))
+                    results_label.text = res
+                
+                with ui.HStack(height=20):
+                    ui.Button("Upload to Edge Impulse", clicked_fn=on_click)
+                
+                with ui.HStack(height=20):
+                    ui.Spacer(width=3)
+                    results_label = ui.Label("", height=20, word_wrap=True)
+                
+
+    def on_shutdown(self):
+        print("[edgeimpulse.dataingestion] Edge Impulse Data Ingestion shutdown")
+
+   
+
+    
